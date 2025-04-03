e459ae86835fab83cd3bb357760c028c257e538c
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-04-03 14:34:32 -0300

    exercise 7.1

---
+++ routed-anecdotes/src/App.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react'
+import {
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+import About from './components/About'
+import AnecdoteList from './components/AnecdoteList'
+import Footer from './components/Footer'
+import CreateAnecdote from './components/CreateAnecdote'
+import Menu from './components/Menu'
+
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  // const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  // const anecdoteById = (id) =>
+  //   anecdotes.find(a => a.id === id)
+
+  // const vote = (id) => {
+  //   const anecdote = anecdoteById(id)
+
+  //   const voted = {
+  //     ...anecdote,
+  //     votes: anecdote.votes + 1
+  //   }
+
+  //   setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  // }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateAnecdote addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
